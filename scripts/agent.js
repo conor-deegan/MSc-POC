a0ff02b9f8fd610feb5af32c7c7fb8cfa54bc93c
Conor Deegan <conorjdeegan@gmail.com>
2022-06-17 19:45:46 +0100

    updating the structure of the project

---
+++ scripts/agent.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const main = async () => {
+    // Get contract addresses
+    let addresses = JSON.parse(fs.readFileSync('contractAddresses.json'));
+
+    // create an instance of the agent contract
+    const agentContract = await (await ethers.getContractFactory('AgentContract')).attach(addresses.agentContract);
+
+    // create an agent
+    await agentContract.create('agent1', 'building1');
+
+    // give the agent a goal
+    await agentContract.addGoal('agent1', 'road3');
+}
+
+main();
