048edcec7fafba4b811f09c53bb8c16ddf02f1f9
Conor Deegan <conorjdeegan@gmail.com>
2022-06-18 21:02:37 +0100

    adding base of run

--- scripts/run.js
+++ scripts/run.js
@@ -1,7 +1,25 @@
 const fs = require('fs');
 
 const main = async () => {
+    // Get contract addresses
+    let addresses = JSON.parse(fs.readFileSync('contractAddresses.json'));
 
+    // create an instance of the agent contract
+    const agentContract = await (await ethers.getContractFactory('AgentContract')).attach(addresses.agentContract);
+
+    // set up epochs
+    const epoch = async () => {
+        // get all agent ids
+        const agentIds = await agentContract.getAll();
+        // loop over each agentId
+        for (const agentId of agentIds) {
+            // logic goes here
+        }
+        // wait 1 second and then recusively call epoch
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        epoch();
+    }
+    epoch();
 }
 
 main();
